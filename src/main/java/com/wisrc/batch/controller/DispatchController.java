3ea8e2a038e7a586fede0dc4f995af27908be8cc
huang <tonyhuang@pateo.global>
2019-02-27 16:47:25 +0800

    调整参数

--- src/main/java/com/wisrc/batch/controller/DispatchController.java
+++ src/main/java/com/wisrc/batch/controller/DispatchController.java
@@ -12,15 +12,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -30,8 +29,8 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @Scope("prototype")
 @Api(description ="批次调度-批次启动管理")
+@Slf4j
 public class DispatchController {
-    private final Logger logger = LoggerFactory.getLogger(DispatchController.class);
 
     @Autowired
     private QuartzSchedulerManager quartzSchedulerManager;
@@ -46,30 +45,29 @@ public class DispatchController {
     @ResponseBody
     @ApiOperation(value = "启动批次", notes = "启动批次时，需要两个参数，一个是域编码，另一个是批次编码")
     @ApiImplicitParams({
-            @ApiImplicitParam(required = true, name = "domain_id", value = "域编码"),
-            @ApiImplicitParam(required = true, name = "batch_id", value = "批次编码"),
+            @ApiImplicitParam(required = true, name = "domainId", value = "域编码"),
+            @ApiImplicitParam(required = true, name = "batchId", value = "批次编码"),
     })
-    public String start(HttpServletResponse response, HttpServletRequest request) {
-
-        String domainId = request.getParameter("domain_id");
-        String batchId = request.getParameter("batch_id");
+    public String start(@RequestParam(value = "domainId") String domainId,
+                        @RequestParam(value = "batchId") String batchId,
+                        HttpServletResponse response) {
 
         if (domainId == null || batchId == null) {
             response.setStatus(421);
-            logger.info("batch id or domain jobKey is null ,batch jobKey is: {}, domain jobKey is: {}", batchId, domainId);
+            log.info("batch id or domain jobKey is null ,batch jobKey is: {}, domain jobKey is: {}", batchId, domainId);
             return Hret.error(421, "domain_id is empty or batch_id is empty", null);
         }
 
         if (BatchStatus.BATCH_STATUS_RUNNING == batchDefineService.getStatus(batchId)) {
             response.setStatus(421);
-            logger.info("批次正在运行中，无法重新启动, 批次号是：{}", batchId);
+            log.info("批次正在运行中，无法重新启动, 批次号是：{}", batchId);
             return Hret.error(421, "批次正在运行中", null);
         }
 
         BatchRunConfDto bconf = batchDefineService.initConf(batchId, domainId);
         RetMsg retMsg = execService.init(bconf);
         if (!retMsg.checkCode()) {
-            logger.info(retMsg.getMessage());
+            log.info(retMsg.getMessage());
             response.setStatus(retMsg.getCode());
             return Hret.error(retMsg);
         }
@@ -77,11 +75,11 @@ public class DispatchController {
         retMsg = batchDefineService.runBatchInit(batchId);
         if (SysStatus.SUCCESS_CODE != retMsg.getCode()) {
             batchDefineService.setStatus(batchId, BatchStatus.BATCH_STATUS_ERROR);
-            logger.info(retMsg.toString());
+            log.info(retMsg.toString());
             response.setStatus(426);
             return Hret.error(retMsg);
         }
-        logger.debug("初始化修改批次状态信息完成，批次号是：{}", batchId);
+        log.debug("初始化修改批次状态信息完成，批次号是：{}", batchId);
 
         // 进度调度依赖关系管理
         // 根据依赖关系,开启任务触发器
@@ -92,10 +90,10 @@ public class DispatchController {
             batchDefineService.setStatus(batchId, BatchStatus.BATCH_STATUS_ERROR);
             return Hret.error(SysStatus.ERROR_CODE, "启动调度器失败", e.getMessage());
         }
-        logger.debug("调度器创建成功，批次号是：{}", batchId);
+        log.debug("调度器创建成功，批次号是：{}", batchId);
 
         quartzSchedulerManager.schedulerStart();
-        logger.info("批次初始化完成，调度服务已启动，批次号是：{}", batchId);
+        log.info("批次初始化完成，调度服务已启动，批次号是：{}", batchId);
         return Hret.success(SysStatus.SUCCESS_CODE, "start batch successfully. batch id is :" + batchId, null);
     }
 }
